7447a59919d9efb531cf5cc8cdf09c4c96f77aa1
Xiaochen Wang <wangxiaochen0@gmail.com>
2017-10-03 14:13:43 +0800

    ngx_http_proxy_connect_tunnel: write do_write variable to debug log

--- ngx_http_proxy_connect_module.c
+++ ngx_http_proxy_connect_module.c
@@ -378,8 +378,9 @@ ngx_http_proxy_connect_tunnel(ngx_http_request_t *r,
     c = r->connection;
     u = ctx->u;
 
-    ngx_log_debug1(NGX_LOG_DEBUG_HTTP, c->log, 0,
-                   "http proxy_connect, fu:%ui", from_upstream);
+    ngx_log_debug2(NGX_LOG_DEBUG_HTTP, c->log, 0,
+                   "http proxy_connect, fu:%ui write:%ui",
+                   from_upstream, do_write);
 
     downstream = c;
     upstream = u->peer.connection;
